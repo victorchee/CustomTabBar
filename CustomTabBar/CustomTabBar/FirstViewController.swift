ef6242e18d4c80cf0391aadb1cd4b7cdf4a444bd
Victor Chee <victor.qhj@gmail.com>
2018-12-20 20:42:23 +0800

    Add project.

---
+++ CustomTabBar/CustomTabBar/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CustomTabBar
+//
+//  Created by Victor Chee on 2018/12/20.
+//  Copyright © 2018 VictorChee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CustomTabBar/CustomTabBar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CustomTabBar
+//
+//  Created by Victor Chee on 2018/12/20.
+//  Copyright © 2018 VictorChee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
